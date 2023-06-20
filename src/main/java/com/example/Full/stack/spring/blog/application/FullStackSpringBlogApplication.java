5b3c54e26b280131352303f9c3328b3998cbe7eb
James Ward <jimmywardmichael@gmail.com>
2023-06-20 18:23:26 -0500

    init push

---
+++ src/main/java/com/example/Full/stack/spring/blog/application/FullStackSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.Full.stack.spring.blog.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackSpringBlogApplication.class, args);
+	}
+
+}
