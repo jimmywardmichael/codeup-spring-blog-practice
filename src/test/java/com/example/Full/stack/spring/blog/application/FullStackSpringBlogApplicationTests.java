fb41f936be6f69d8bcd5d4daafe459aff4dbc89b
James Ward <jimmywardmichael@gmail.com>
2023-06-22 10:10:26 -0500

    commented out tests.java

--- src/test/java/com/example/Full/stack/spring/blog/application/FullStackSpringBlogApplicationTests.java
+++ src/test/java/com/example/Full/stack/spring/blog/application/FullStackSpringBlogApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.Full.stack.spring.blog.application;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FullStackSpringBlogApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.Full.stack.spring.blog.application;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FullStackSpringBlogApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
